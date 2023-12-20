5a75baa81e86b37df6b907660d896d876e15edf0
Woo Jia Hao <woojiahao1234@gmail.com>
2023-12-20 12:53:24 +0800

    Day 20 base

---
+++ lib/aoc/y2023/day_20.ex
@@ -0,0 +1,26 @@
+defmodule AOC.Y2023.Day20 do
+  @moduledoc """
+  Part 1
+
+  Part 2
+  """
+  use AOC.Solution
+
+  @impl true
+  def load_data() do
+    Data.load_day(20)
+    |> Enum.map(fn line ->
+
+    end)
+  end
+
+  @impl true
+  def part_one(data) do
+
+  end
+
+  @impl true
+  def part_two(data) do
+
+  end
+end
