384013f9bb6964de41ab8ddad144b19c7c7ca950
Woo Jia Hao <woojiahao1234@gmail.com>
2024-10-12 13:33:57 +0100

    2022 day 7

--- lib/aoc/y2022/day_6.ex
+++ lib/aoc/y2022/day_6.ex
@@ -3,18 +3,67 @@ defmodule AOC.Y2022.Day6 do
 
   use AOC.Solution
 
+  @window_size_1 4
+  @window_size_2 14
+
   @impl true
   def load_data() do
-    Data.load_day(2022, 6)
+    Data.load_day(2022, 6) |> hd() |> String.graphemes()
   end
 
   @impl true
-  def part_one(_data) do
-    :not_implemented
+  def part_one(data) do
+    data
+    |> Enum.with_index()
+    |> Enum.reduce_while(%{}, fn
+      {v, idx}, win when idx < @window_size_1 ->
+        {:cont, Map.put(win, v, Map.get(win, v, 0) + 1)}
+
+      {v, idx}, win ->
+        out = Enum.at(data, idx - @window_size_1)
+
+        if out == v do
+          {:cont, win}
+        else
+          updated_win =
+            win
+            |> Map.put(v, Map.get(win, v, 0) + 1)
+            |> Map.put(out, max(0, Map.get(win, out, 0) - 1))
+
+          if Enum.all?(updated_win, &(elem(&1, 1) <= 1)) do
+            {:halt, idx + 1}
+          else
+            {:cont, updated_win}
+          end
+        end
+    end)
   end
 
   @impl true
-  def part_two(_data) do
-    :not_implemented
+  def part_two(data) do
+    data
+    |> Enum.with_index()
+    |> Enum.reduce_while(%{}, fn
+      {v, idx}, win when idx < @window_size_2 ->
+        {:cont, Map.put(win, v, Map.get(win, v, 0) + 1)}
+
+      {v, idx}, win ->
+        out = Enum.at(data, idx - @window_size_2)
+
+        if out == v do
+          {:cont, win}
+        else
+          updated_win =
+            win
+            |> Map.put(v, Map.get(win, v, 0) + 1)
+            |> Map.put(out, max(0, Map.get(win, out, 0) - 1))
+
+          if Enum.all?(updated_win, &(elem(&1, 1) <= 1)) do
+            {:halt, idx + 1}
+          else
+            {:cont, updated_win}
+          end
+        end
+    end)
   end
 end
--- lib/aoc/y2022/day_7.ex
+++ lib/aoc/y2022/day_7.ex
@@ -2,19 +2,62 @@ defmodule AOC.Y2022.Day7 do
   @moduledoc false
 
   use AOC.Solution
+  import Utils.General, [:map_sum]
 
   @impl true
   def load_data() do
     Data.load_day(2022, 7)
+    |> Enum.reduce({%{}, []}, fn
+      "$ cd ..", {acc, cur_dir} ->
+        {acc, Enum.drop(cur_dir, -1)}
+
+      "$ cd " <> cd, {acc, cur_dir} ->
+        {acc, cur_dir ++ [cd]}
+
+      "$ ls", acc ->
+        acc
+
+      "dir " <> dir, {acc, cur_dir} ->
+        {acc
+         |> Map.put_new(cur_dir ++ [dir], [])
+         |> Map.put(cur_dir, Map.get(acc, cur_dir, []) ++ [{:folder, dir}]), cur_dir}
+
+      file, {acc, cur_dir} ->
+        [size, name] = String.split(file)
+
+        {Map.put(
+           acc,
+           cur_dir,
+           Map.get(acc, cur_dir, []) ++ [{:file, name, String.to_integer(size)}]
+         ), cur_dir}
+    end)
+    |> elem(0)
   end
 
   @impl true
-  def part_one(_data) do
-    :not_implemented
+  def part_one(data) do
+    data
+    |> Enum.map(fn {folder, _files} -> folder_size(folder, data) end)
+    |> Enum.filter(&(&1 <= 100_000))
+    |> Enum.sum()
   end
 
   @impl true
-  def part_two(_data) do
-    :not_implemented
+  def part_two(data) do
+    root_size = folder_size(["/"], data)
+
+    data
+    |> Enum.map(fn {folder, _files} -> folder_size(folder, data) end)
+    |> Enum.filter(&(&1 >= root_size - 40_000_000))
+    |> Enum.min()
+  end
+
+  defp folder_size(cur, folder) when not is_map_key(folder, cur), do: 0
+
+  defp folder_size(cur, folder) do
+    map_sum(folder[cur], fn
+      {:folder, folder_name} -> folder_size(cur ++ [folder_name], folder)
+      {:file, _file_name, file_size} -> file_size
+    end)
   end
 end
