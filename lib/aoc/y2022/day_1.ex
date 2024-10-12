870be027b926111bf0afa51ab4102895fa78991a
Woo Jia Hao <woojiahao1234@gmail.com>
2024-10-12 09:38:08 +0100

    2022 day 1

--- lib/aoc/y2022/day_1.ex
+++ lib/aoc/y2022/day_1.ex
@@ -5,16 +5,23 @@ defmodule AOC.Y2022.Day1 do
 
   @impl true
   def load_data() do
-    Data.load_day(2022, 1)
+    Data.load_day(2022, 1, "\n\n")
+    |> Enum.map(fn elf ->
+      elf |> String.split("\n") |> Enum.map(&String.to_integer/1) |> Enum.sum()
+    end)
   end
 
   @impl true
-  def part_one(_data) do
-    :not_implemented
+  def part_one(data) do
+    data
+    |> Enum.max()
   end
 
   @impl true
-  def part_two(_data) do
-    :not_implemented
+  def part_two(data) do
+    data
+    |> Enum.sort(:desc)
+    |> Enum.take(3)
+    |> Enum.sum()
   end
 end
