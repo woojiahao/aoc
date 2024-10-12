1b1fd07b5944a6c77eba60d5c7be8696d8da87d6
Woo Jia Hao <woojiahao1234@gmail.com>
2024-10-12 10:15:24 +0100

    2022 day 3

--- lib/aoc/y2022/day_3.ex
+++ lib/aoc/y2022/day_3.ex
@@ -2,6 +2,8 @@ defmodule AOC.Y2022.Day3 do
   @moduledoc false
 
   use AOC.Solution
+  import Utils.String, [:ord]
+  import Utils.Set, [:list_intersection]
 
   @impl true
   def load_data() do
@@ -9,12 +11,38 @@ defmodule AOC.Y2022.Day3 do
   end
 
   @impl true
-  def part_one(_data) do
-    :not_implemented
+  def part_one(data) do
+    data
+    |> Enum.map(fn line ->
+      len = String.length(line)
+      half = div(len, 2)
+      {String.slice(line, 0..(half - 1)), String.slice(line, half..len)}
+    end)
+    |> Enum.map(fn {first, second} ->
+      list_intersection(String.to_charlist(first), String.to_charlist(second))
+    end)
+    |> Enum.map(&hd/1)
+    |> Enum.map(&to_string/1)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.map(&priority/1)
+    |> Enum.sum()
   end
 
   @impl true
-  def part_two(_data) do
-    :not_implemented
+  def part_two(data) do
+    data
+    |> Enum.chunk_every(3)
+    |> Enum.map(fn [a, b, c] ->
+      list_intersection(String.to_charlist(a), String.to_charlist(b))
+      |> list_intersection(String.to_charlist(c))
+    end)
+    |> Enum.map(&hd/1)
+    |> Enum.map(&to_string/1)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.map(&priority/1)
+    |> Enum.sum()
   end
+
+  defp priority(value) when value in ?a..?z, do: value - ord("a") + 1
+  defp priority(value) when value in ?A..?Z, do: value - ord("A") + 27
 end
--- lib/utils/string.ex
+++ lib/utils/string.ex
@@ -1,6 +1,7 @@
 defmodule Utils.String do
   @moduledoc false
 
+  @spec ord(binary()) :: integer()
   def ord(s) do
     :binary.first(s)
   end
