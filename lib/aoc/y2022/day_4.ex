14eaa5e1d2af4ab4c3243a1d3cfbee2d9f6f04b9
Woo Jia Hao <woojiahao1234@gmail.com>
2024-10-12 10:23:20 +0100

    2022 day 4

--- lib/aoc/y2022/day_4.ex
+++ lib/aoc/y2022/day_4.ex
@@ -6,15 +6,23 @@ defmodule AOC.Y2022.Day4 do
   @impl true
   def load_data() do
     Data.load_day(2022, 4)
+    |> Enum.map(fn line -> String.split(line, ",") end)
+    |> Enum.map(fn [first, second] ->
+      [a, b] = first |> String.split("-") |> Enum.map(&String.to_integer/1)
+      [c, d] = second |> String.split("-") |> Enum.map(&String.to_integer/1)
+      {{a, b}, {c, d}}
+    end)
   end
 
   @impl true
-  def part_one(_data) do
-    :not_implemented
+  def part_one(data) do
+    data
+    |> Enum.count(fn {{a, b}, {c, d}} -> (a >= c and b <= d) or (c >= a and d <= b) end)
   end
 
   @impl true
-  def part_two(_data) do
-    :not_implemented
+  def part_two(data) do
+    data
+    |> Enum.count(fn {{a, b}, {c, d}} -> a <= d and c <= b end)
   end
 end
