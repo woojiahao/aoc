b5cc3074c514953b53ade43eb3a4557007624636
Woo Jia Hao <woojiahao1234@gmail.com>
2024-10-14 09:13:23 +0100

    2022 day 13

--- lib/aoc/y2022/day_13.ex
+++ lib/aoc/y2022/day_13.ex
@@ -2,19 +2,53 @@ defmodule AOC.Y2022.Day13 do
   @moduledoc false
 
   use AOC.Solution
+  import Utils.General, [:map_sum, :map_product]
+
+  @dividers [[[[2]]], [[[6]]]]
 
   @impl true
   def load_data() do
-    Data.load_day(2022, 13)
+    Data.load_day(2022, 13, "\n\n")
+    |> Enum.map(fn pair ->
+      pair |> String.split("\n") |> Enum.map(&Code.eval_string(&1)) |> Enum.map(&elem(&1, 0))
+    end)
   end
 
   @impl true
-  def part_one(_data) do
-    :not_implemented
+  def part_one(data) do
+    data
+    |> Enum.with_index(1)
+    |> Enum.filter(fn {[a, b], _idx} -> compare(a, b) == :lt end)
+    |> map_sum(fn {_data, idx} -> idx end)
   end
 
   @impl true
-  def part_two(_data) do
-    :not_implemented
+  def part_two(data) do
+    @dividers
+    |> Kernel.++(data)
+    |> Enum.flat_map(& &1)
+    |> Enum.sort(fn a, b -> compare(a, b) in [:lt, :eq] end)
+    |> Enum.with_index(1)
+    |> Enum.filter(fn {v, _idx} -> v == [[2]] or v == [[6]] end)
+    |> map_product(fn {_v, idx} -> idx end)
+  end
+
+  defp compare([], []), do: :eq
+  defp compare([], _r), do: :lt
+  defp compare(_l, []), do: :gt
+
+  defp compare([l | lr], [r | rr]) when is_integer(l) and is_integer(r) do
+    cond do
+      l < r -> :lt
+      l > r -> :gt
+      true -> compare(lr, rr)
+    end
+  end
+
+  defp compare([l | lr], [r | rr]) do
+    case compare(List.wrap(l), List.wrap(r)) do
+      :eq -> compare(lr, rr)
+      v -> v
+    end
   end
 end
--- lib/utils/array.ex
+++ lib/utils/array.ex
@@ -6,8 +6,4 @@ defmodule Utils.Array do
     |> List.zip()
     |> Enum.map(&Tuple.to_list/1)
   end
-
-  def to_grid_string(arr) do
-   
-  end
 end
