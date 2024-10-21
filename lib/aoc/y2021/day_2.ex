7e7fc3c254740fb690c4a6449acb2442e6730746
Woo Jia Hao <woojiahao1234@gmail.com>
2024-10-21 20:45:52 +0100

    2021 day 2

--- lib/aoc/y2021/day_2.ex
+++ lib/aoc/y2021/day_2.ex
@@ -9,12 +9,24 @@ defmodule AOC.Y2021.Day2 do
   end
 
   @impl true
-  def part_one(_data) do
-    :not_implemented
+  def part_one(data) do
+    data
+    |> Enum.reduce({0, 0}, fn
+      "forward " <> x, {a, b} -> {a + String.to_integer(x), b}
+      "down " <> x, {a, b} -> {a, b + String.to_integer(x)}
+      "up " <> x, {a, b} -> {a, b - String.to_integer(x)}
+    end)
+    |> then(fn {a, b} -> a * b end)
   end
 
   @impl true
-  def part_two(_data) do
-    :not_implemented
+  def part_two(data) do
+    data
+    |> Enum.reduce({0, 0, 0}, fn
+      "down " <> v, {x, y, a} -> {x, y, a + String.to_integer(v)}
+      "up " <> v, {x, y, a} -> {x, y, a - String.to_integer(v)}
+      "forward " <> v, {x, y, a} -> {x + String.to_integer(v), y + String.to_integer(v) * a, a}
+    end)
+    |> then(fn {a, b, _} -> a * b end)
   end
 end
