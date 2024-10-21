d90be38440b35121ae9af27b35cd48c224a212e7
Woo Jia Hao <woojiahao1234@gmail.com>
2024-10-21 20:33:34 +0100

    add 2021

---
+++ lib/aoc/y2021/day_6.ex
@@ -0,0 +1,20 @@
+defmodule AOC.Y2021.Day6 do
+  @moduledoc false
+
+  use AOC.Solution
+
+  @impl true
+  def load_data() do
+    Data.load_day(2021, 6)
+  end
+
+  @impl true
+  def part_one(_data) do
+    :not_implemented
+  end
+
+  @impl true
+  def part_two(_data) do
+    :not_implemented
+  end
+end
---
+++ lib/aoc/y2021/day_9.ex
@@ -0,0 +1,20 @@
+defmodule AOC.Y2021.Day9 do
+  @moduledoc false
+
+  use AOC.Solution
+
+  @impl true
+  def load_data() do
+    Data.load_day(2021, 9)
+  end
+
+  @impl true
+  def part_one(_data) do
+    :not_implemented
+  end
+
+  @impl true
+  def part_two(_data) do
+    :not_implemented
+  end
+end
