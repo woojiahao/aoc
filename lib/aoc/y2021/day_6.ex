03162ab4a488cc426d219c080d96830f8bd8709a
Woo Jia Hao <woojiahao1234@gmail.com>
2024-10-23 00:40:00 +0100

    2021 day 6

--- lib/aoc/y2021/day_6.ex
+++ lib/aoc/y2021/day_6.ex
@@ -2,19 +2,39 @@ defmodule AOC.Y2021.Day6 do
   @moduledoc false
 
   use AOC.Solution
+  import Utils.General, [:map_sum]
 
   @impl true
   def load_data() do
     Data.load_day(2021, 6)
+    |> hd()
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.frequencies()
   end
 
   @impl true
-  def part_one(_data) do
-    :not_implemented
+  def part_one(data) do
+    simulate(data, 80)
   end
 
   @impl true
-  def part_two(_data) do
-    :not_implemented
+  def part_two(data) do
+    simulate(data, 256)
+  end
+
+  defp simulate(population, 0), do: map_sum(population, fn {_, v} -> v end)
+
+  defp simulate(population, days_remaining) do
+    population
+    |> Enum.map(fn {k, v} -> {k - 1, v} end)
+    |> Map.new()
+    |> then(fn up ->
+      up
+      |> Map.put(6, Map.get(up, 6, 0) + Map.get(up, -1, 0))
+      |> Map.put(8, Map.get(up, -1, 0))
+      |> Map.delete(-1)
+    end)
+    |> simulate(days_remaining - 1)
   end
 end
