e4d31be6aa9acf846557556f90978998eafc4479
Woo Jia Hao <woojiahao1234@gmail.com>
2024-10-23 23:56:25 +0100

    2021 day 7

--- lib/aoc/y2021/day_7.ex
+++ lib/aoc/y2021/day_7.ex
@@ -1,20 +1,37 @@
 defmodule AOC.Y2021.Day7 do
   @moduledoc false
 
+  require Integer
   use AOC.Solution
+  import Utils.General, [:map_sum, :map_min]
 
   @impl true
   def load_data() do
     Data.load_day(2021, 7)
+    |> hd()
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
   end
 
   @impl true
-  def part_one(_data) do
-    :not_implemented
+  def part_one(data) do
+    map_min(data, &calculate_fuel(&1, data))
   end
 
   @impl true
-  def part_two(_data) do
-    :not_implemented
+  def part_two(data) do
+    {left, right} = Enum.min_max(data)
+    map_min(left..right, &calculate_fuel_cumulative(&1, data))
+  end
+
+  defp calculate_fuel(dest, crabs) do
+    map_sum(crabs, fn crab -> abs(crab - dest) end)
+  end
+
+  defp calculate_fuel_cumulative(dest, crabs) do
+    map_sum(crabs, fn crab ->
+      cost = abs(crab - dest)
+      div(cost * (cost + 1), 2)
+    end)
   end
 end
