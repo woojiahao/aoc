ccf322254e6e0f2699646081989f460b51ce81ea
Woo Jia Hao <woojiahao1234@gmail.com>
2024-12-01 09:53:38 +0000

    2024 day 1

--- lib/aoc/y2024/day_1.ex
+++ lib/aoc/y2024/day_1.ex
@@ -6,15 +6,24 @@ defmodule AOC.Y2024.Day1 do
   @impl true
   def load_data() do
     Data.load_day(2024, 1)
+    |> Enum.map(&String.split(&1, ~r/\s+/, trim: true))
+    |> Enum.map(fn [a, b] -> {String.to_integer(a), String.to_integer(b)} end)
+    |> Enum.reduce({[], []}, fn {a, b}, {l, r} -> {l ++ [a], r ++ [b]} end)
   end
 
   @impl true
-  def part_one(_data) do
-    :not_implemented
+  def part_one({l, r}) do
+    l
+    |> Enum.sort()
+    |> Enum.zip(Enum.sort(r))
+    |> General.map_sum(fn {a, b} -> abs(a - b) end)
   end
 
   @impl true
-  def part_two(_data) do
-    :not_implemented
+  def part_two({l, r}) do
+    Enum.frequencies(r)
+    |> then(fn freq ->
+      General.map_sum(l, fn a -> a * Map.get(freq, a, 0) end)
+    end)
   end
 end
