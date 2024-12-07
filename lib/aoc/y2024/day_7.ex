7453c3559a34566f79ecfabfb9b837ae01f580d9
Woo Jia Hao <woojiahao1234@gmail.com>
2024-12-07 08:23:45 +0000

    refactor

--- lib/aoc/y2024/day_7.ex
+++ lib/aoc/y2024/day_7.ex
@@ -6,43 +6,43 @@ defmodule AOC.Y2024.Day7 do
   @impl true
   def load_data() do
     Data.load_day(2024, 7)
-    |> Enum.map(fn line -> String.split(line, ":") end)
+    |> Enum.map(fn line -> String.split(line, ": ") end)
     |> Enum.map(fn [test_value, numbers] ->
       {String.to_integer(test_value),
-       numbers |> String.split(" ", trim: true) |> Enum.map(&String.to_integer/1)}
+       numbers |> String.split(" ") |> Enum.map(&String.to_integer/1)}
     end)
   end
 
   @impl true
   def part_one(data) do
-    data
-    |> Enum.filter(fn {test_value, numbers} -> form_test_value?(numbers, test_value) end)
-    |> General.map_sum(fn {test_value, _} -> test_value end)
+    solve(data, false)
   end
 
   @impl true
   def part_two(data) do
+    solve(data, true)
+  end
+
+  defp solve(data, has_concat) do
     data
-    |> Enum.filter(fn {test_value, numbers} -> form_test_value2?(numbers, test_value) end)
+    |> Enum.filter(fn {test_value, numbers} ->
+      form_test_value?(numbers, test_value, has_concat)
+    end)
     |> General.map_sum(fn {test_value, _} -> test_value end)
   end
 
-  defp form_test_value?([acc | _], test_value) when acc > test_value, do: false
-  defp form_test_value?([test_value], test_value), do: true
-  defp form_test_value?([_], _), do: false
+  defp form_test_value?([acc | _], test_value, _) when acc > test_value, do: false
+  defp form_test_value?([test_value], test_value, _), do: true
+  defp form_test_value?([_], _, _), do: false
 
-  defp form_test_value?([a | [b | rest]], test_value),
-    do:
-      form_test_value?([a * b | rest], test_value) or
-        form_test_value?([a + b | rest], test_value)
+  defp form_test_value?([a | [b | rest]], test_value, false) do
+    form_test_value?([a * b | rest], test_value, false) or
+      form_test_value?([a + b | rest], test_value, false)
+  end
 
-  defp form_test_value2?([acc | _], test_value) when acc > test_value, do: false
-  defp form_test_value2?([test_value], test_value), do: true
-  defp form_test_value2?([_], _), do: false
-
-  defp form_test_value2?([a | [b | rest]], test_value),
-    do:
-      form_test_value2?([a * b | rest], test_value) or
-        form_test_value2?([a + b | rest], test_value) or
-        form_test_value2?([String.to_integer("#{a}#{b}") | rest], test_value)
+  defp form_test_value?([a | [b | rest]], test_value, true) do
+    form_test_value?([a * b | rest], test_value, true) or
+      form_test_value?([a + b | rest], test_value, true) or
+      form_test_value?([String.to_integer("#{a}#{b}") | rest], test_value, true)
+  end
 end
