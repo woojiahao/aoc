9949e57d5911a322f0e47a1dc8a45a5b0f786e3c
Woo Jia Hao <woojiahao1234@gmail.com>
2024-12-11 18:14:34 +0800

    2024 day 9

--- lib/aoc/y2024/day_9.ex
+++ lib/aoc/y2024/day_9.ex
@@ -13,45 +13,69 @@ defmodule AOC.Y2024.Day9 do
 
   @impl true
   def part_one(data) do
-    disks =
-      data
-      |> Enum.reduce({[], false, 0}, fn
-        v, {acc, false, i} ->
-          {acc ++ List.duplicate(i, v), true, i + 1}
+    data
+    |> Enum.reduce({[], false, 0}, fn
+      v, {acc, false, i} ->
+        {acc ++ List.duplicate(i, v), true, i + 1}
 
-        v, {acc, true, i} ->
-          {acc ++ List.duplicate(nil, v), false, i}
-      end)
-      |> elem(0)
-      |> Arrays.new(implementation: Arrays.Implementations.ErlangArray)
-
-    compact(disks, 0, Arrays.size(disks) - 1)
-    |> Enum.reject(&is_nil/1)
-    |> Enum.with_index()
-    |> General.map_sum(fn {v, i} -> v * i end)
+      v, {acc, true, i} ->
+        {acc ++ List.duplicate(nil, v), false, i}
+    end)
+    |> elem(0)
+    |> Arrays.new(implementation: Arrays.Implementations.ErlangArray)
+    |> then(fn disks ->
+      disks
+      |> compact(0, Arrays.size(disks) - 1)
+      |> Enum.reject(&is_nil/1)
+      |> Enum.with_index()
+      |> General.map_sum(fn {v, i} -> v * i end)
+    end)
   end
 
   @impl true
   def part_two(data) do
-    files =
-      data
-      |> Enum.with_index()
-      |> Enum.group_by(fn
-        {_, k} when rem(k, 2) == 0 -> :file
-        _ -> :free
-      end)
-      |> IO.inspect()
-
-    files
+    data
+    |> Enum.with_index()
+    |> Enum.map(fn
+      {v, k} when rem(k, 2) == 0 -> %{type: :file, free: 0, data: List.duplicate(div(k, 2), v)}
+      {v, _} -> %{type: :free, free: v, data: []}
+    end)
+    |> Arrays.new(implementation: Arrays.Implementations.ErlangArray)
+    |> then(fn disk ->
+      first_fit(disk, Arrays.size(disk) - 1)
+      |> Enum.flat_map(fn %{data: data, free: free} -> data ++ List.duplicate(0, free) end)
+    end)
+    |> Enum.with_index()
+    |> General.map_sum(fn {v, i} -> v * i end)
   end
 
-  defp first_fit(free, [file | file_rest], disk) do
-    {_, free_slot_idx} = free_slot = Enum.find(free, fn slot -> slot >= file end)
+  defp first_fit(disk, -1), do: disk
 
-    if is_nil(free_slot) do
-      first_fit(free, file_rest, disk)
+  defp first_fit(disk, r) do
+    %{type: type, data: data} = disk[r]
+
+    if type == :free do
+      first_fit(disk, r - 1)
     else
-      first_fit(free, file_rest, disk)
+      l =
+        disk
+        |> Enum.find_index(fn %{type: type, free: free} ->
+          type == :free and free >= length(data)
+        end)
+
+      if is_nil(l) or l >= r do
+        first_fit(disk, r - 1)
+      else
+        %{free: free_space, data: free_data} = disk[l]
+
+        updated_disk =
+          put_in(disk[l], %{disk[l] | free: free_space - length(data), data: free_data ++ data})
+          |> then(fn new_disk ->
+            put_in(new_disk[r], %{new_disk[r] | free: length(data), data: []})
+          end)
+
+        first_fit(updated_disk, r - 1)
+      end
     end
   end
 
