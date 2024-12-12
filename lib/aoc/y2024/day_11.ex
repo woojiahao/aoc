7a2f9f60fe8979dbdd2e42c21047c203de22e899
Woo Jia Hao <woojiahao1234@gmail.com>
2024-12-12 16:49:08 +0800

    2024 day 11

--- lib/aoc/y2024/day_11.ex
+++ lib/aoc/y2024/day_11.ex
@@ -1,20 +1,61 @@
 defmodule AOC.Y2024.Day11 do
   @moduledoc false
 
+  require Integer
   use AOC.Solution
 
   @impl true
   def load_data() do
     Data.load_day(2024, 11)
+    |> then(fn [line] ->
+      line |> String.split(" ", trim: true) |> Enum.map(&String.to_integer/1)
+    end)
   end
 
   @impl true
-  def part_one(_data) do
-    :not_implemented
+  def part_one(rocks) do
+    solve(rocks, 25)
   end
 
   @impl true
-  def part_two(_data) do
-    :not_implemented
+  def part_two(rocks) do
+    solve(rocks, 75)
   end
+
+  defp solve(rocks, n) do
+    1..n
+    |> Enum.reduce(Enum.frequencies(rocks), fn _, rock_freq ->
+      rock_freq
+      |> Enum.reduce(%{}, fn {v, f}, new_rock_freq ->
+        blink(v)
+        |> Enum.reduce(new_rock_freq, fn new_v, acc ->
+          Map.update(acc, new_v, f, fn prev -> prev + f end)
+        end)
+      end)
+    end)
+    |> General.map_sum(fn {_, f} -> f end)
+  end
+
+  defp blink(rock) do
+    cond do
+      rock == 0 -> [1]
+      Integer.is_even(digits(rock)) -> split_num(rock)
+      true -> [rock * 2024]
+    end
+  end
+
+  defp digits(num), do: num |> Integer.digits() |> length()
+
+  defp split_num(num) do
+    v = Integer.digits(num)
+
+    v
+    |> Enum.split(div(length(v), 2))
+    |> then(fn {a, b} ->
+      [list_to_integer(a, 0), list_to_integer(b, 0)]
+    end)
+  end
+
+  defp list_to_integer([], acc), do: acc
+  defp list_to_integer([d | rest], acc), do: list_to_integer(rest, acc * 10 + d)
 end
