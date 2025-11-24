e24e2225d7fb2e385b77f1a8f08c5aaf9d6ebdf9
Jiahao, Woo <woojiahao1234@gmail.com>
2025-11-24 09:29:40 +0800

    Update to latest Elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule AOC.MixProject do
     [
       app: :aoc,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.19.3",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
