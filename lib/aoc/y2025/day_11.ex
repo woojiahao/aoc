5a475d40f71e099caca618764d5008c58a5ef7a5
Jiahao, Woo <woojiahao1234@gmail.com>
2025-12-07 00:28:38 +0800

    Add base files for 2025

---
+++ lib/aoc/y2025/day_11.ex
@@ -0,0 +1,21 @@
+defmodule AOC.Y2025.Day11 do
+  @moduledoc false
+
+  use AOC.Solution, year: 2025, day: 11
+
+  @impl true
+  def load_data(data, _opts) do
+    data
+    |> String.split("\n")
+  end
+
+  @impl true
+  def part_one(_data, _opts) do
+    :not_implemented
+  end
+
+  @impl true
+  def part_two(_data, _opts) do
+    :not_implemented
+  end
+end
