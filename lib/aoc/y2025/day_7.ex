f9d3b2d036ab474dafa9552199f25a5eee2d7f86
Jiahao, Woo <woojiahao1234@gmail.com>
2025-12-07 17:34:36 +0800

    Solve day 7 2025

--- lib/aoc/y2025/day_7.ex
+++ lib/aoc/y2025/day_7.ex
@@ -5,17 +5,65 @@ defmodule AOC.Y2025.Day7 do
 
   @impl true
   def load_data(data, _opts) do
-    data
-    |> String.split("\n")
+    {grid, m, n} =
+      data
+      |> String.split("\n")
+      |> Data.parse_as_grid()
+
+    tachyon_pos = grid |> Enum.find(fn {_, v} -> v == "S" end) |> elem(0)
+
+    splitters =
+      grid
+      |> Enum.filter(fn {_, v} -> v == "^" end)
+      |> Enum.map(&elem(&1, 0))
+      |> MapSet.new()
+
+    {tachyon_pos, splitters, m, n}
   end
 
   @impl true
-  def part_one(_data, _opts) do
-    :not_implemented
+  def part_one({tachyon_pos, splitters, m, _}, _opts) do
+    1..(m - 1)
+    |> Enum.reduce({%{tachyon_pos => 1}, 0}, fn row, {beams, splits} ->
+      {new_beams, add_splits} = calculate_beams(beams, splitters, row)
+      {new_beams, splits + add_splits}
+    end)
+    |> elem(1)
   end
 
   @impl true
-  def part_two(_data, _opts) do
-    :not_implemented
+  def part_two({tachyon_pos, splitters, m, _}, _opts) do
+    1..(m - 1)
+    |> Enum.reduce({%{tachyon_pos => 1}, 0}, fn row, {beams, splits} ->
+      {new_beams, add_splits} = calculate_beams(beams, splitters, row)
+      {new_beams, splits + add_splits}
+    end)
+    |> elem(0)
+    |> Enum.sum_by(fn {_, f} -> f end)
+  end
+
+  defp calculate_beams(cur_beams, splitters, row),
+    do: calculate_beams(cur_beams, splitters, row, %{}, 0)
+
+  defp calculate_beams(cur_beams, _, _, beams, splits) when map_size(cur_beams) == 0,
+    do: {beams, splits}
+
+  defp calculate_beams(cur_beams, splitters, row, beams, splits) do
+    {{_, c} = key, f} = Enum.at(cur_beams, 0)
+    tail = Map.delete(cur_beams, key)
+
+    keys =
+      if MapSet.member?(splitters, {row, c}),
+        do: [{row, c - 1}, {row, c + 1}],
+        else: [{row, c}]
+
+    new_split =
+      if MapSet.member?(splitters, {row, c}),
+        do: splits + 1,
+        else: splits
+
+    new_beams = keys |> Enum.reduce(beams, fn key, acc -> Map.update(acc, key, f, &(&1 + f)) end)
+
+    calculate_beams(tail, splitters, row, new_beams, new_split)
   end
 end
