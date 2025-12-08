54bb2bb525219ff191ddd47659f8d98de580d5f4
Jiahao, Woo <woojiahao1234@gmail.com>
2025-12-08 11:28:18 +0800

    Solve day 5 2025

--- lib/aoc/y2025/day_5.ex
+++ lib/aoc/y2025/day_5.ex
@@ -5,17 +5,39 @@ defmodule AOC.Y2025.Day5 do
 
   @impl true
   def load_data(data, _opts) do
-    data
-    |> String.split("\n")
+    [ingredient_ranges_raw, ingredient_ids_raw] = String.split(data, "\n\n")
+
+    ingredient_ranges =
+      ingredient_ranges_raw
+      |> String.split("\n")
+      |> Enum.map(&String.split(&1, "-"))
+      |> Enum.map(&Enum.map(&1, fn v -> String.to_integer(v) end))
+      |> Enum.sort()
+      |> merge_intervals([])
+
+    ingredient_ids = ingredient_ids_raw |> String.split("\n") |> Enum.map(&String.to_integer/1)
+
+    {ingredient_ranges, ingredient_ids}
   end
 
   @impl true
-  def part_one(_data, _opts) do
-    :not_implemented
+  def part_one({ingredient_ranges, ingredient_ids}, _opts) do
+    Enum.count(ingredient_ids, &within_range?(&1, ingredient_ranges))
   end
 
   @impl true
-  def part_two(_data, _opts) do
-    :not_implemented
+  def part_two({ingredient_ranges, _}, _opts) do
+    Enum.sum_by(ingredient_ranges, fn [s, e] -> e - s + 1 end)
   end
+
+  defp merge_intervals([], acc), do: Enum.reverse(acc)
+  defp merge_intervals([h | rest], []), do: merge_intervals(rest, [h])
+
+  defp merge_intervals([[s, e] | rest], [[a, b] | o]) when s <= b,
+    do: merge_intervals(rest, [[a, max(e, b)] | o])
+
+  defp merge_intervals([h | rest], acc), do: merge_intervals(rest, [h | acc])
+
+  defp within_range?(ingredient_id, ingredient_ranges),
+    do: Enum.any?(ingredient_ranges, fn [s, e] -> ingredient_id >= s and ingredient_id <= e end)
 end
