b1b56fabb5477a537308817a0c69c4840fc03b81
Jiahao, Woo <woojiahao1234@gmail.com>
2025-12-08 12:55:49 +0800

    Solve day 4 2025

--- lib/aoc/y2025/day_4.ex
+++ lib/aoc/y2025/day_4.ex
@@ -3,19 +3,84 @@ defmodule AOC.Y2025.Day4 do
 
   use AOC.Solution, year: 2025, day: 4
 
+  @dirs [
+    {-1, 0},
+    {1, 0},
+    {0, -1},
+    {0, 1},
+    {-1, -1},
+    {-1, 1},
+    {1, -1},
+    {1, 1}
+  ]
+
   @impl true
   def load_data(data, _opts) do
-    data
-    |> String.split("\n")
+    {grid, m, n} =
+      data
+      |> String.split("\n")
+      |> Data.parse_as_grid()
+
+    rolls =
+      grid |> Enum.filter(&(elem(&1, 1) == "@")) |> Enum.map(&elem(&1, 0)) |> MapSet.new()
+
+    {rolls, m, n}
   end
 
   @impl true
-  def part_one(_data, _opts) do
-    :not_implemented
+  def part_one({rolls, m, n}, _opts) do
+    rolls
+    |> Enum.map(&neighbors(&1, rolls, m, n))
+    |> Enum.count(&(length(&1) < 4))
   end
 
   @impl true
-  def part_two(_data, _opts) do
-    :not_implemented
+  def part_two({rolls, m, n}, _opts) do
+    rolls
+    |> Map.new(fn coord -> {coord, coord |> neighbors(rolls, m, n) |> MapSet.new()} end)
+    |> remove_rolls()
+  end
+
+  defp neighbors({r, c}, rolls, m, n) when is_struct(rolls, MapSet) do
+    @dirs
+    |> Enum.map(fn {dr, dc} -> {r + dr, c + dc} end)
+    |> Enum.reject(fn {dr, dc} -> dr < 0 or dr >= m or dc < 0 or dc >= n end)
+    |> Enum.filter(&MapSet.member?(rolls, &1))
+  end
+
+  defp neighbors(coord, rolls, m, n) do
+    rolls_keys = rolls |> Map.keys() |> MapSet.new()
+    neighbors(coord, rolls_keys, m, n)
+  end
+
+  defp remove_rolls(rolls) do
+    frontier =
+      rolls
+      |> Enum.filter(&(MapSet.size(elem(&1, 1)) < 4))
+      |> Enum.map(&elem(&1, 0))
+
+    process_frontier(rolls, frontier, MapSet.new(frontier))
+  end
+
+  defp process_frontier(_, [], visited), do: MapSet.size(visited)
+
+  defp process_frontier(rolls, frontier, visited) do
+    {rolls, new_frontier, visited} =
+      Enum.reduce(frontier, {rolls, [], visited}, fn coord, {g, f, v} ->
+        neighbors = g[coord]
+
+        Enum.reduce(neighbors, {g, f, v}, fn neighbor, {gg, ff, vv} ->
+          new_set = MapSet.delete(gg[neighbor], coord)
+          gg = Map.put(gg, neighbor, new_set)
+
+          if MapSet.size(new_set) < 4 and not MapSet.member?(vv, neighbor) do
+            {gg, [neighbor | ff], MapSet.put(vv, neighbor)}
+          else
+            {gg, ff, vv}
+          end
+        end)
+      end)
+
+    process_frontier(rolls, new_frontier, visited)
   end
 end
