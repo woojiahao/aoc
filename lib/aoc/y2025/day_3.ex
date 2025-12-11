68351e4795a64b8b1c99e28cc664076e8f01ec06
Jiahao, Woo <woojiahao1234@gmail.com>
2025-12-11 20:11:20 +0800

    Solve day 3 2025

--- lib/aoc/y2025/day_3.ex
+++ lib/aoc/y2025/day_3.ex
@@ -7,15 +7,39 @@ defmodule AOC.Y2025.Day3 do
   def load_data(data, _opts) do
     data
     |> String.split("\n")
+    |> Enum.map(
+      &(String.graphemes(&1)
+        |> Enum.map(fn v -> String.to_integer(v) end)
+        |> :array.from_list())
+    )
   end
 
   @impl true
-  def part_one(_data, _opts) do
-    :not_implemented
+  def part_one(data, _opts) do
+    Enum.sum_by(data, &solve(&1, 2))
   end
 
   @impl true
-  def part_two(_data, _opts) do
-    :not_implemented
+  def part_two(data, _opts) do
+    Enum.sum_by(data, &solve(&1, 12))
   end
+
+  defp solve(joltages, take), do: solve(joltages, 0, take, 0)
+  defp solve(_, _, 0, acc), do: acc
+
+  defp solve(joltages, l, take, acc) do
+    i =
+      largest_joltage(joltages, l, :array.size(joltages) - take + 1, l, :array.get(l, joltages))
+
+    solve(joltages, i + 1, take - 1, acc * 10 + :array.get(i, joltages))
+  end
+
+  defp largest_joltage(_, l, l, idx, _), do: idx
+
+  defp largest_joltage(joltages, l, r, idx, cur),
+    do:
+      if(:array.get(l, joltages) > cur,
+        do: largest_joltage(joltages, l + 1, r, l, :array.get(l, joltages)),
+        else: largest_joltage(joltages, l + 1, r, idx, cur)
+      )
 end
